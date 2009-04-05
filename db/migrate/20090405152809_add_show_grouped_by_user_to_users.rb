1e29cd147433b8114b22c847aff858fa3e118afa
wulfovitch <bugtracking@wulfi.net>
2009-04-05 17:46:16 +0200

    small bugfix in the last created migration

--- db/migrate/20090405152809_add_show_grouped_by_user_to_users.rb
+++ db/migrate/20090405152809_add_show_grouped_by_user_to_users.rb
@@ -1,6 +1,6 @@
 class AddShowGroupedByUserToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :show_grouped_by_user, :boolean, :default => 'false'
+    add_column :users, :show_grouped_by_user, :boolean, :default => false
   end
 
   def self.down
