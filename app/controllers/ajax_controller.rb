164242aef9b1da1f8d5ece68c339de453c2986ff
wulfovitch <github@wulfi.net>
2011-11-13 15:00:07 +0100

    autocomplete now searches alphabetically

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -6,6 +6,7 @@ class AjaxController < ApplicationController
     else
       collections = Collection.all
     end
+    collections = collections.sort_by(&:collection_title)
     list = collections.map {|c| Hash[ id: c.id, label: c.collection_title, name: c.collection_title]}
     render json: list
   end
